aa5bb31d611f2317ed94997539c9985bd9e740da
Webbynode <felipe@webbynode.com>
2009-04-09 17:11:11 -0300

    Fixed installation script to install correct gems

--- webby_config.sh
+++ webby_config.sh
@@ -1,19 +1,15 @@
 sudo gem install -v 1.9.0 liquid
 sudo gem install tzinfo
 
-cp config/database.yml.tmpl config/database.yml
-
-mkdir log
-chown www-data log
-
 echo "
 production:
     adapter: mysql
     database: ${WC_APP_NAME}
     username: root
-    password: 
+    password: ${WC_DB_PASSWORD}
     host: localhost
     encoding: utf8
 " > config/database.yml
 
 rake db:bootstrap RAILS_ENV=production
+chown www-data log
