13660640a1b49d056994ca39a88c90c4b152dab6
Webbynode <felipe@webbynode.com>
2009-04-09 15:59:29 -0300

    Added webby_config script to deploy on Webbynode

---
+++ webby_config.sh
@@ -0,0 +1,18 @@
+sudo gem install tzinfo
+
+cp config/database.yml.tmpl config/database.yml
+
+mkdir log
+chown www-data log
+
+echo "
+production:
+    adapter: mysql
+    database: ${WC_APP_NAME}
+    username: root
+    password: 
+    host: localhost
+    encoding: utf8
+" > config/database.yml
+
+rake db:bootstrap
