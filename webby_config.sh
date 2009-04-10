2ad547f89245129dcf0c7e7c836ad88b2849eedd
Webbynode <felipe@webbynode.com>
2009-04-10 19:46:47 -0300

    Added PostgreSQL support to webby_config.sh

--- webby_config.sh
+++ webby_config.sh
@@ -1,15 +1,31 @@
 sudo gem install -v 1.9.0 liquid
 sudo gem install tzinfo
 
+if [ "${WC_DB_ENGINE}" == "mysql" ]; then
 echo "
 production:
     adapter: mysql
     database: ${WC_APP_NAME}
-    username: root
+    username: ${WC_APP_NAME}
     password: ${WC_DB_PASSWORD}
     host: localhost
     encoding: utf8
 " > config/database.yml
+fi
+
+if [ "${WC_DB_ENGINE}" == "postgressql" ]; then
+echo "
+production:
+    adapter: postgresql
+    encoding: unicode
+    database: ${WC_APP_NAME}
+    pool: 5
+    username: ${WC_APP_NAME}
+    password: ${WC_DB_PASSWORD}
+    host: localhost
+    port: 5432
+" > config/database.yml
+fi
 
 rake db:bootstrap RAILS_ENV=production
 chown www-data log
