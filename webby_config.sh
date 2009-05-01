b7a6122d0a1717b8d1427d20e913e5a4c8b852d2
Travis Graham <tgraham@smithandhammaker.com>
2009-04-30 21:40:38 -0400

    mephisto config rework

--- webby_config.sh
+++ webby_config.sh
@@ -4,30 +4,31 @@ sudo gem install tzinfo
 
 echo WC_DB_ENGINE=${WC_DB_ENGINE}
 
+echo "
+login: &login
+  adapter: ${WC_DB_ENGINE}
+  database: ${WC_APP_NAME}
+  username: ${WC_APP_NAME}
+  password: ${WC_DB_PASSWORD}
+  host: localhost
+" > config/database.yml
+
 if [ "${WC_DB_ENGINE}" == "mysql" ]; then
 echo "
 production:
-    adapter: mysql
-    database: ${WC_APP_NAME}
-    username: ${WC_APP_NAME}
-    password: ${WC_DB_PASSWORD}
-    host: localhost
-    encoding: utf8
-" > config/database.yml
+  <<: *login
+  encoding: utf8
+" >> config/database.yml
 fi
 
 if [ "${WC_DB_ENGINE}" == "postgresql" ]; then
 echo "
 production:
-    adapter: postgresql
-    encoding: unicode
-    database: ${WC_APP_NAME}
-    pool: 5
-    username: ${WC_APP_NAME}
-    password: ${WC_DB_PASSWORD}
-    host: localhost
-    port: 5432
-" > config/database.yml
+  <<: *login
+  encoding: unicode
+  pool: 5
+  port: 5432
+" >> config/database.yml
 fi
 
 rake db:bootstrap RAILS_ENV=production
